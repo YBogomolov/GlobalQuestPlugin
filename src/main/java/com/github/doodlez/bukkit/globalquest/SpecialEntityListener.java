a18f5d61e573b4d43a190386746ada26afd5c2d6
Yuriy Bogomolov <cronus.cpp@gmail.com>
2011-07-14 14:30:26 +0300

    Added SpecialEntityListener. Set Herobrine to deal double damage and receive half of the damage.

--- src/main/java/com/github/doodlez/bukkit/globalquest/SpecialEntityListener.java
+++ src/main/java/com/github/doodlez/bukkit/globalquest/SpecialEntityListener.java
@@ -18,47 +18,50 @@ public class SpecialEntityListener extends EntityListener {
      */
     @Override
     public void onEntityDamage(EntityDamageEvent event) {
-        // Let's set appropriae damage values for Herobrine if he attacks player
-        // or he is attacked by player.
-        if (event.getCause().equals(EntityDamageEvent.DamageCause.ENTITY_ATTACK)) {
-            EntityDamageByEntityEvent e = (EntityDamageByEntityEvent)event;
-            //Check to see if the damager and damaged are players
-            if (e.getDamager() instanceof Player && e.getEntity() instanceof Player) {
-                Player damager = (Player)e.getDamager();
-                Player damagee = (Player)e.getEntity();
+        // Only process when both damager and damagee are Entities.
+        if (event instanceof EntityDamageByEntityEvent) {
+            // Let's set appropriae damage values for Herobrine if he attacks player
+            // or he is attacked by player.
+            if (event.getCause().equals(EntityDamageEvent.DamageCause.ENTITY_ATTACK)) {
+                EntityDamageByEntityEvent e = (EntityDamageByEntityEvent)event;
+                //Check to see if the damager and damaged are players
+                if (e.getDamager() instanceof Player && e.getEntity() instanceof Player) {
+                    Player damager = (Player)e.getDamager();
+                    Player damagee = (Player)e.getEntity();
 
-                // Original damage value:
-                int previousDamage = e.getDamage();
+                    // Original damage value:
+                    int previousDamage = e.getDamage();
 
-                // Now we have options:
-                // 1. If Herobrine is damager, he sould deal DOUBLE damage:
-                if (damager.getName().equals("")) {
-                    e.setDamage(previousDamage * 2);
-                    return;
-                }
+                    // Now we have options:
+                    // 1. If Herobrine is damager, he sould deal DOUBLE damage:
+                    if (damager.getName().equals("")) {
+                        e.setDamage(previousDamage * 2);
+                        return;
+                    }
 
-                // 2. If Herobrine is the damagee, hesould receive HALF of damage:
-                if (damagee.getName().equals("")) {
-                    e.setDamage(previousDamage / 2);
-                    return;
+                    // 2. If Herobrine is the damagee, hesould receive HALF of damage:
+                    if (damagee.getName().equals("")) {
+                        e.setDamage(previousDamage / 2);
+                        return;
+                    }
                 }
             }
-        }
 
-        // The same — for fall damage:
-        if ((event.getCause().equals(EntityDamageEvent.DamageCause.FALL))
-            || (event.getCause().equals(EntityDamageEvent.DamageCause.FIRE))
-            || (event.getCause().equals(EntityDamageEvent.DamageCause.FIRE_TICK))) {
-            EntityDamageByEntityEvent e = (EntityDamageByEntityEvent)event;
-            if (e.getEntity() instanceof Player) {
-                Player damagee = (Player)e.getEntity();
+            // The same — for fall damage:
+            if ((event.getCause().equals(EntityDamageEvent.DamageCause.FALL))
+                || (event.getCause().equals(EntityDamageEvent.DamageCause.FIRE))
+                || (event.getCause().equals(EntityDamageEvent.DamageCause.FIRE_TICK))) {
+                EntityDamageByEntityEvent e = (EntityDamageByEntityEvent)event;
+                if (e.getEntity() instanceof Player) {
+                    Player damagee = (Player)e.getEntity();
 
-                // Original damage value:
-                int previousDamage = e.getDamage();
+                    // Original damage value:
+                    int previousDamage = e.getDamage();
 
-                 // If Herobrine is the damagee, hesould receive HALF of damage:
-                if (damagee.getName().equals("")) {
-                    e.setDamage(previousDamage / 2);
+                     // If Herobrine is the damagee, hesould receive HALF of damage:
+                    if (damagee.getName().equals("")) {
+                        e.setDamage(previousDamage / 2);
+                    }
                 }
             }
         }
